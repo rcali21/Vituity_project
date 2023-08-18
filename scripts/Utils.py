ea373758ea91711b5b9eaf383c2e6bfc4ec76a19
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-08-18 11:09:19 -0400

    Added re for email extraction

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -68,6 +68,10 @@ def extract_msg_fields(message):
             extracted_data["bill_amount"] = 1234
             ### C. ADD DATE OF SERVICE
             extracted_data["date_of_service"] = today
+            email = re.search(r'[\w.-]+@[\w.-]+', pid_fields[13])
+            # insert email by grouping the entire match if the re works properly
+            extracted_data['patient_email_address'] = email.group(0) if email else None
+            
 
         # Extract data from MSH section
         msh_segment = [line for line in lines if line.startswith("MSH")]

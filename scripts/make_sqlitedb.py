940b8e9d3982e55e669bb8092c1b326eda07d33a
Ryan Cali <ryancali@PaulThoRyansMBP.fios-router.home>
2023-08-17 21:22:41 -0400

    black for formatting

--- scripts/make_sqlitedb.py
+++ scripts/make_sqlitedb.py
@@ -2,33 +2,37 @@ import os
 import pandas as pd
 import sqlite3
 import datetime as dt
+
 today = dt.date.today().strftime("%d_%m_%Y")
 
+
 def main():
-    
-    base_dir = '../Archive/Modified/'
+
+    base_dir = "../Archive/Modified/"
 
     file_dir = os.listdir(base_dir)
-    
+
     # Filter for ADT CSV files
-    adt_csv = [f for f in file_dir if f.endswith('.csv') and 'ADT' in f]
-    
+    adt_csv = [f for f in file_dir if f.endswith(".csv") and "ADT" in f]
+
     if adt_csv:
-        adt_csv_file = os.path.join(base_dir, adt_csv[0])  # Combine base directory with filename
+        adt_csv_file = os.path.join(
+            base_dir, adt_csv[0]
+        )  # Combine base directory with filename
         df = pd.read_csv(adt_csv_file)
 
-        bonus_dir = '../Archive/Bonus/'
+        bonus_dir = "../Archive/Bonus/"
         os.mkdir(bonus_dir)
 
         # Create db
-        conn = sqlite3.connect(os.path.join(bonus_dir, 'ADT.db'))
+        conn = sqlite3.connect(os.path.join(bonus_dir, "ADT.db"))
 
         # write existing adt_df to sqlite db
-        df.to_sql('table_name', conn, if_exists='replace', index=False)
+        df.to_sql("table_name", conn, if_exists="replace", index=False)
 
         # Close
         conn.close()
 
+
 if __name__ == "__main__":
     main()
-
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -4,12 +4,13 @@ import shutil
 import datetime as dt
 from Utils import parse_and_save_csv, extract_msg_fields, name_concat, sum_bills
 
+
 def main():
     today = dt.date.today().strftime("%d_%m_%Y")
-    directory = os.chdir('../')
-    df = pd.read_csv('sampledata.csv')
-    target_files = ['ADT_sample.txt', 'Sample_ORU.txt', 'sampledata.csv']
-    dirs_to_make = ['Archive', 'Archive/Original', 'Archive/Modified']
+    directory = os.chdir("../")
+    df = pd.read_csv("sampledata.csv")
+    target_files = ["ADT_sample.txt", "Sample_ORU.txt", "sampledata.csv"]
+    dirs_to_make = ["Archive", "Archive/Original", "Archive/Modified"]
 
     # basic checks for dirs and files
     for dir in dirs_to_make:
@@ -17,22 +18,20 @@ def main():
             os.mkdir(dir)
     for file in target_files:
         if not os.path.exists(file):
-            print(f'File {file} does not exist. Ensure all files are present.')
+            print(f"File {file} does not exist. Ensure all files are present.")
             break
         else:
-            print(f'File {file} exists. Copying file to Archive/Original/ folder...')
-            shutil.copy(file, './Archive/Original/')
-
-    
+            print(f"File {file} exists. Copying file to Archive/Original/ folder...")
+            shutil.copy(file, "./Archive/Original/")
 
     # Call all of our functions from utils
-    
-    adt_df = parse_and_save_csv(df, 'ADT')
-    oru_df = parse_and_save_csv(df, 'ORU')
 
-    adt_parsed = extract_msg_fields('./Archive/Original/ADT_sample.txt')
+    adt_df = parse_and_save_csv(df, "ADT")
+    oru_df = parse_and_save_csv(df, "ORU")
 
-    oru_parsed = extract_msg_fields('./Archive/Original/Sample_ORU.txt')
+    adt_parsed = extract_msg_fields("./Archive/Original/ADT_sample.txt")
+
+    oru_parsed = extract_msg_fields("./Archive/Original/Sample_ORU.txt")
 
     adt_df_from_txt = pd.concat([adt_df, pd.DataFrame([adt_parsed])], ignore_index=True)
     oru_df_from_txt = pd.concat([oru_df, pd.DataFrame([oru_parsed])], ignore_index=True)
@@ -40,13 +39,15 @@ def main():
     name_concat(adt_df_from_txt)
     name_concat(oru_df_from_txt)
 
-    adt_df_from_txt.to_csv(f'./Archive/Modified/ADT_{today}_Modified_file.csv', index=False)
-    oru_df_from_txt.to_csv(f'./Archive/Modified/ORU_{today}_Modified_file.csv', index=False)
+    adt_df_from_txt.to_csv(
+        f"./Archive/Modified/ADT_{today}_Modified_file.csv", index=False
+    )
+    oru_df_from_txt.to_csv(
+        f"./Archive/Modified/ORU_{today}_Modified_file.csv", index=False
+    )
 
     sum_bills(df)
 
 
-
 if __name__ == "__main__":
     main()
-
